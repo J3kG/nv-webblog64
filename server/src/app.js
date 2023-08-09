f9a462a3d5afa47c8e09906db7411a11d8898594
Chartnarong <rinex1394@gmail.com>
2023-08-09 11:29:09 +0700

    page 54

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,11 @@ app.get('/status', function (req,res){
 
 let port = 8081
 
+app.get('/hello/:name', function(req,res){
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+})
+
 app.listen(port, function(){
 
     console.log('server running on ' + port)
