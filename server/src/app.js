c137f44f899fec462d1c35dcbdd6fbd82f7c6faa
Chartnarong <rinex1394@gmail.com>
2023-08-22 20:13:58 +0700

    finished ch04

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
 
 const app = express()
 
- 
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/status', function (req,res){
 
@@ -17,6 +19,26 @@ let port = 8081
 app.get('/hello/:name', function(req,res){
     console.log('hello - ' + req.params.name)
     res.send('say hello with ' + req.params.name)
+})
+
+app.get('/user/:userId', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้' + req.params.userId)
+})
+
+app.get('/users', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+})
+
+app.post('/user', function(req,res){
+    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+})
+
+app.put('/user/:userId', function(req,res){
+    res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + " " + JSON.stringify(req.body.name))
+})
+
+app.delete('/user/:userId', function(req, res){
+    res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
 })
 
 app.listen(port, function(){
