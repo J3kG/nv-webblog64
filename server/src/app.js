a0d78c9c9fc836fb7e899033ef7de506c2e69cd0
Chartnarong <rinex1394@gmail.com>
2023-10-16 23:19:39 +0700

    fifth commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,27 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+let cors = require('cors')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
 
 require('./route')(app)
 
-app.get('/status', function (req,res){
-
-    res.send('Hello nodejs server')
-
-})
 
  
 
-let port = 8081
+let port = process.env.PORT || config.port
 
-app.get('/hello/:name', function(req,res){
-    console.log('hello - ' + req.params.name)
-    res.send('say hello with ' + req.params.name)
+sequelize.sync({force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
 
-app.listen(port, function(){
 
-    console.log('server running on ' + port)
-
-})
