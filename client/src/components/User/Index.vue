085e36173b0c3c20cc0f6f372357cd58574b220e
Chartnarong <rinex1394@gmail.com>
2023-10-17 22:58:28 +0700

    whatever commit

--- client/src/components/User/Index.vue
+++ client/src/components/User/Index.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
         <h1>Get All Users</h1>
+        <div v-if="users.length">
+            <div>จำนวนผู้ใช้งาน{{ users.length }} </div>
+            <div>id: {{ users[0].id }}</div>
+            <div>ชื่อ-นามสกุล {{ users[0].name }} - {{ user[0].lastname }}</div>
+            <div>email: {{ users[0].email }}</div>
+            <div>password: {{ users[0].password }}</div>
+        </div>
     </div>
 </template>
 <script>
+import UserService from '@/services/UserService'
+
 export default {
-    created () {
-        this.$http.get('http://localhost:8081/uesrs')
-            .then(function (response) {
-                console.log(response)
-            })
+    data () {
+        return {
+            users: []
+        }
+    },
+    async created () {
+        let results = (await UserService.index()).data
+        console.log(results)
     }
 }
 </script>
